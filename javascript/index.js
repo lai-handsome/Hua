f60d48f9a9a898c341356cbbe3b7b3e5d272f783
lai-handsome <844312631@qq.com>
2018-10-04 20:40:14 +0800

    今天的任务,做了好多好多

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+//1.导入模块的公用部分
+!function($){
+	$('.top-banner').load('header.html .top-banner');
+	$('.Header').load('header.html');
+	$('.main').load('main.html');
+	$('.footer').load('footer.html');
+}(jQuery);
